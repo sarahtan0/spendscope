cbcc69babad825cb43548708f45f4652184231b4
Sarah <sarahitan8@gmail.com>
2025-06-26 23:43:59 -0700

    returned db on first load

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import LogModel from "./models/log";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
+//async because find() takes time to retrieve from db
+app.get("/", async (req, res) => {
+    //returns the db to user
+    const notes = await LogModel.find().exec();
+    res.status(200).json(notes);
 })
 
 export default app;
