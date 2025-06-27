6a099e698c8b77628d5a32352553666438f9ec55
Sarah <sarahitan8@gmail.com>
2025-06-27 00:14:56 -0700

    added error handling upon server load

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import LogModel from "./models/log";
 
 const app = express();
 
 //async because find() takes time to retrieve from db
-app.get("/", async (req, res) => {
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
     //returns the db to user
-    const notes = await LogModel.find().exec();
-    res.status(200).json(notes);
-})
+    try{
+        const notes = await LogModel.find().exec();
+        res.status(200).json(notes);
+    } 
+    catch (error) {
+        next(error);
+    }
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occured";
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({error: errorMessage});
+});
 
 export default app;
