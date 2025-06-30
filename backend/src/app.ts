377b2909f621e49b55af1b64006d557a9231f41c
Sarah <sarahitan8@gmail.com>
2025-06-30 13:00:24 -0700

    added deployed frontend to cors origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3001", "https://spendscope-drab.vercel.app/"],
     credentials: true,
 }))
 
