345279b952b8f32fc21ee9cedde3ce54bcf72719
Sarah <sarahitan8@gmail.com>
2025-06-30 22:30:27 -0700

    Added a spinner while loading logs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import {LogObject} from "./models/Log";
 import Log from "./components/Log";
-import { Button, Stack } from 'react-bootstrap';
+import { Button, Spinner, Stack } from 'react-bootstrap';
 import * as LogsApi from "./networks/logs_api";
 import AddLogModal from "./components/AddEditLogModal";
 
@@ -10,11 +10,23 @@ function App() {
   const [logs, setLogs] = useState<LogObject[]>([]);
   const [showAddLog, setShowAddLog] = useState(false);
   const [currentlyEditing, setCurrentlyEditing] = useState<LogObject|null>(null);
+  const [loadingLogs, setLoadingLogs] = useState(true);
+  const [errorLoadingLogs, setErrorLoadingLogs] = useState(false);
 
   useEffect(() => {
     async function getLogs(){
-      const logs = await LogsApi.getLogs();
-      setLogs(logs);
+      try{
+        setErrorLoadingLogs(false);
+        setLoadingLogs(true);
+        const logs = await LogsApi.getLogs();
+        setLoadingLogs(false);
+        setLogs(logs);
+      } catch (error) {
+        console.error(error);
+        setErrorLoadingLogs(true);
+      } finally {
+        setLoadingLogs(false);
+      }
     }
 
     getLogs();
@@ -24,41 +36,58 @@ function App() {
 
   return (
     <>
-      <Stack gap={3}>
-        {logs.map(log => (
-            <Log 
-              onLogClicked={(log) => setCurrentlyEditing(log)}
-              log={log}
-              deleteClicked={() => {
-                LogsApi.deleteLog(log._id);
-                setLogs(logs.filter(existingLog => existingLog._id !== log._id));
-              }}
-            />
-          )
-        )}
-      </Stack>
-      <Button onClick={() => setShowAddLog(true)}>ADD</Button>
-
-      {showAddLog &&
-        <AddLogModal
-          onDismiss={() => setShowAddLog(false)}
-          onLogSaved={(newLogs) => {
-            setLogs([...logs, newLogs])
-            setShowAddLog(false);
-          }}
-          logToEdit={null}
-        />
+      {loadingLogs &&
+        <div>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      }
+      {errorLoadingLogs && 
+        <div>
+          <h2>Could not load your logs. Please try again</h2>
+        </div>
       }
 
-      {currentlyEditing && 
-        <AddLogModal
-          onDismiss={() => setCurrentlyEditing(null)}
-          onLogSaved={(updatedLog) => {
-            setCurrentlyEditing(null);
-            setLogs(logs.map(currentLog => currentLog._id === updatedLog._id ? updatedLog : currentLog));
-          }}
-          logToEdit={currentlyEditing}
-        />
+      {!errorLoadingLogs &&
+        <>
+          <Stack gap={3}>
+            {logs.map(log => (
+                <Log 
+                  onLogClicked={(log) => setCurrentlyEditing(log)}
+                  log={log}
+                  deleteClicked={() => {
+                    LogsApi.deleteLog(log._id);
+                    setLogs(logs.filter(existingLog => existingLog._id !== log._id));
+                  }}
+                />
+              )
+            )}
+          </Stack>
+          <Button onClick={() => setShowAddLog(true)}>ADD</Button>
+
+          {showAddLog &&
+            <AddLogModal
+              onDismiss={() => setShowAddLog(false)}
+              onLogSaved={(newLogs) => {
+                setLogs([...logs, newLogs])
+                setShowAddLog(false);
+              }}
+              logToEdit={null}
+            />
+          }
+
+          {currentlyEditing && 
+            <AddLogModal
+              onDismiss={() => setCurrentlyEditing(null)}
+              onLogSaved={(updatedLog) => {
+                setCurrentlyEditing(null);
+                setLogs(logs.map(currentLog => currentLog._id === updatedLog._id ? updatedLog : currentLog));
+              }}
+              logToEdit={currentlyEditing}
+            />
+          }
+        </>
       }
     </>
   );
