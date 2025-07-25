e2a2b19a3da269042219f6ff12cc45236165a7d9
Sarah <sarahitan8@gmail.com>
2025-07-25 15:59:29 -0700

    implemented login, signup, logout in frontend network

--- frontend/src/models/User.ts
+++ frontend/src/models/User.ts
@@ -1,6 +1,6 @@
 export interface User {
     _id: string,
-    user: string,
+    username: string,
     email: string,
     password: string,
 }
--- frontend/src/networks/logs_api.ts
+++ frontend/src/networks/logs_api.ts
@@ -17,8 +17,50 @@ async function fetchData (input: RequestInfo, init?: RequestInit){
 }
 
 export async function getLoggedInUser(): Promise<User> {
-    const response = await fetchData("/users", {method: "GET"});
+    const response = await fetchData("/users", {
+        method: "GET",
+        credentials: "include",
+    });
     return response.json();
+}
+
+export interface SignUpCredentials {
+    username?: string,
+    email?: string,
+    password?: string,
+}
+
+export async function signUp(credentials: SignUpCredentials): Promise<User> {
+    const response = await fetchData("/users/signup", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(credentials)
+    });
+    return response.json();
+}
+
+export interface LoginCredentials {
+    username: string,
+    password?: string
+}
+
+export async function login(credentials: LoginCredentials): Promise<User> {
+    const response = await fetchData("users/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(credentials),
+    });
+    return response.json();
+}
+
+export async function logout() {
+    await fetchData("users/logout", {method: "POST"});
 }
 
 export async function getLogs():Promise<LogObject[]> {
